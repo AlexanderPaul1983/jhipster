94e5e2e96ed398f17a146905f22dc733470db987
AlexanderPaul1983 <alexanderpaul@gmx.de>
2025-02-06 19:42:35 +0100

    added category

--- src/main/webapp/app/entities/entity.routes.ts
+++ src/main/webapp/app/entities/entity.routes.ts
@@ -6,6 +6,11 @@ const routes: Routes = [
     data: { pageTitle: 'Authorities' },
     loadChildren: () => import('./admin/authority/authority.routes'),
   },
+  {
+    path: 'category',
+    data: { pageTitle: 'Categories' },
+    loadChildren: () => import('./category/category.routes'),
+  },
   /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
 ];
 
